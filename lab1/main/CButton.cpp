4e59a17706d0f6fabda593e9206b5f824ab89be4
Karlo Lučan <karlo.lucan@fer.hr>
2026-03-26 16:52:09 +0100

    LAB1 init

---
+++ lab1/main/CButton.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "CButton.h"
+
+CButton::CButton(int port){
+
+    //ToDo::
+    //BOOT is GPIO0 (HIGH when released, LOW when pressed)
+}
+
+void CButton::tick(){
+    //ToDo
+}
